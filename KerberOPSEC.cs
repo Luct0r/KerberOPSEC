661866c934ee2e018d3f6a5ec0238064e84ac71e
Luct0r <43627224+Luct0r@users.noreply.github.com>
2022-01-07 09:28:09 -0800

    Create KerberOPSEC.cs

---
+++ KerberOPSEC.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Text.RegularExpressions;
+using System.Security.Principal;
+using System.Collections.Generic;
+
+namespace KerberOPSEC
+{
+	class KerberOPSEC
+	{
+		public static void GetDN()
+		{
+			
+		}
+	}
+}
